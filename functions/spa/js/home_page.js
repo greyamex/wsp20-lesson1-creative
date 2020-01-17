262bca6a528334ea724b32840ffee62772637cdb
Evan Bigbie <wbigbie@gmail.com>
2020-01-16 18:36:15 -0600

    home: headers centered, bebas neue, padding

--- functions/spa/js/home_page.js
+++ functions/spa/js/home_page.js
@@ -1,22 +1,22 @@
 function home_page() {
     pageContent.innerHTML = `
-    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active" data-interval="10000">
-        <img src="brownstone-02.jpg" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item" data-interval="2000">
-        <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-item active">
+        <img src="images/manhattan-01.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="images/brownstone-01.webp" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/manhattan-02.webp" class="d-block w-100" alt="...">
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
